38c217cedc383fb11befa6a6b952a30bca7a33bc
huobazi <huobazi@gmail.com>
2012-07-28 11:41:15 +0800

    ex1.rb

---
+++ ex1.rb
@@ -0,0 +1,9 @@
+puts "Hello World!"
+puts "Hello Again"
+puts "I like type this."
+puts "This is fun."
+puts 'Yay! Printing'
+puts "I'd much rather you 'not'."
+puts 'I "said" do not touch this.'
+puts 'My name is Marble'
+#puts ' hahaha'
