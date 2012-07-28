62ab173aa9e4b55221ecd6e566c5bb6e22a25094
huobazi <huobazi@gmail.com>
2012-07-28 20:09:45 +0800

    ex10.rb

---
+++ ex10.rb
@@ -0,0 +1,17 @@
+tabby_cat     = "\t I'm tabbed in."
+persian_cat   = "I'm split \non aline."
+backslash_cat = "I'm \\ a \\ cat."
+
+fat_cat = <<MY_FAT_CAT
+I'll do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+MY_FAT_CAT
+
+puts tabby_cat
+puts persian_cat
+puts backslash_cat
+puts fat_cat
+
+
