4173a99e9d2f4a211d04483c662edda8391902ff
huobazi <huobazi@gmail.com>
2012-07-28 12:11:24 +0800

    ex2.rb

---
+++ ex2.rb
@@ -0,0 +1,11 @@
+# A comment, this is so you can read you program later
+# Anything after the # is ignored by Ruby.
+#
+
+puts "I could have code like this" # add the comment after is ignored
+
+# You can also use a comment to "disable " or comment out a piece of code:
+# print "This won't run."
+#
+
+puts "This will run."
